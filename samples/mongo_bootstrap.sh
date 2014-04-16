bdedd1238400c8d99fff9995dffd5aa3a870796c
Ryan Brooks <ryanbrooksis@gmail.com>
2014-04-16 22:36:44 +0100

    ADdding samples and gitignore:

---
+++ samples/mongo_bootstrap.sh
@@ -0,0 +1,253 @@
+
+
+curl -X POST -H "Content-Type: application/json" -d '{
+ "description": "Follow the obvious crack to the right of Launching Pad. Well protected if you have got cams.",
+ "grade": "E2 5c",
+ "length": "11m",
+ "name": "Holy, Holy, Holy",
+ "stars": "3",
+ "loc": [
+ 53.123578,
+ -4.102395
+  ]
+}' http://localhost:3000/api/climbs
+
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "description": "Ascend the groove to reach the slab, then skirt the left-hand edge following smooth footholds. Keep right of the easy ground for a spicy finish.",
+  "grade": "E1 5a",
+  "length": "14m",
+  "name": "Launching Pad",
+  "stars": "1",
+  "loc": [
+  53.123578,
+  -4.102395
+  ]
+}' http://localhost:3000/api/climbs
+
+
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E1 5a",
+        "name": "Youre not in Poland Now",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E1 5b",
+        "name": "God Help the Sailors.....",
+        "stars": 0
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E2 6a",
+        "name": "Sombrero Fallout",
+        "stars": 0
+}' http://localhost:3000/api/climbs
+
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E6 6c",
+        "name": "Truncheon Meat",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "HS 4c",
+        "name": "The Cracks",
+        "stars": 3
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E2 5c",
+        "name": "GBH",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "VS 4c",
+        "name": "Lorraine",
+        "stars": 2
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "HVS 5a",
+        "name": "Lorraine Variation",
+        "stars": 2
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E9 7a",
+        "name": "Trauma",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "VS 5b",
+        "name": "Direct Route",
+        "stars": 3
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "HVS 5a",
+        "name": "Famine",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E1 5b",
+        "name": "Super Direct",
+        "stars": 3
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E3 6a",
+        "name": "Zeta",
+        "stars": 2
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "HVS 5a",
+        "name": "Diagonal",
+        "stars": 3
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "HVS 5a",
+        "name": "Crosstie",
+        "stars": 1
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "grade": "E3 5c",
+        "name": "Stairway to Heaven",
+        "stars": 2
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Launching Pad"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
+
+curl -X POST -H "Content-Type: application/json" -d '{
+        "loc": [
+             53.085311,
+            -4.051440
+        ],
+        "name": "Holy, Holy, Holy"
+}' http://localhost:3000/api/climbs
